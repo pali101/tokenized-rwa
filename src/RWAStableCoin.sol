dac77139e228d4f02f543319507a844b3a59b597
Aashish Paliwal <aashish.paliwal@live.com>
2025-03-05 12:30:31 +0530

    fix: import format

--- src/RWAStableCoin.sol
+++ src/RWAStableCoin.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
-import "./RWAToken.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC1155Receiver} from "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {RWAToken} from "./RWAToken.sol";
 
 contract RWAStableCoin is ERC20, Ownable, IERC1155Receiver {
     RWAToken public rwaToken;
