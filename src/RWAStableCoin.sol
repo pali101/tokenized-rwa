e1b407b26d8c0b8ed66d707e3be0d3f1bca717d5
Aashish Paliwal <aashish.paliwal@live.com>
2025-03-05 10:27:08 +0530

    refactor: stableCoin issue amount formula for better precision

--- src/RWAStableCoin.sol
+++ src/RWAStableCoin.sol
@@ -13,80 +13,45 @@ contract RWAStableCoin is ERC20, Ownable {
     // property id => number of shares locked
     mapping(uint256 => uint256) public collateralLocked;
 
-    event StablecoinMinted(
-        address indexed user,
-        uint256 propertyId,
-        uint256 amount
-    );
+    event StablecoinMinted(address indexed user, uint256 propertyId, uint256 amount);
     event StablecoinBurned(address indexed user, uint256 amount);
     event RWARefunded(address indexed user, uint256 propertyId, uint256 shares);
-    event LiquidationTriggered(
-        uint256 indexed propertyId,
-        uint256 liquidatedShares,
-        uint256 burnedStablecoins
-    );
+    event LiquidationTriggered(uint256 indexed propertyId, uint256 liquidatedShares, uint256 burnedStablecoins);
 
-    constructor(
-        address rwaTokenAddress
-    ) ERC20("RWA Stablecoin", "RSC") Ownable(msg.sender) {
+    constructor(address rwaTokenAddress) ERC20("RWA Stablecoin", "RSC") Ownable(msg.sender) {
         rwaToken = RWAToken(rwaTokenAddress);
     }
 
     function mintStableCoin(uint256 propertyId, uint256 shares) external {
-        require(
-            rwaToken.balanceOf(msg.sender, propertyId) >= shares,
-            "Not enough RWA tokens"
-        );
+        require(rwaToken.balanceOf(msg.sender, propertyId) >= shares, "Not enough RWA tokens");
         require(rwaToken.whitelistedInvestors(msg.sender), "Not whitelisted");
 
-        (, uint256 propertyValue, uint256 numberOfShares) = rwaToken.properties(
-            propertyId
-        );
-        uint256 shareValue = propertyValue / numberOfShares;
+        (, uint256 propertyValue, uint256 numberOfShares) = rwaToken.properties(propertyId);
+        // uint256 shareValue = propertyValue / numberOfShares;
         // here shares is used for number of shares being pegged for this particular property
-        uint256 stableCoinAmount = (shareValue * shares) / COLLATERAL_RATIO;
+        // (propertyValue * sharesToBePegged) / (totalShares * COLLATERAL_RATIO)
+        uint256 stableCoinAmount = (propertyValue * shares) / (numberOfShares * COLLATERAL_RATIO);
 
         require(stableCoinAmount > 0, "Minting amount too low");
 
-        rwaToken.safeTransferFrom(
-            msg.sender,
-            address(this),
-            propertyId,
-            shares,
-            ""
-        );
+        rwaToken.safeTransferFrom(msg.sender, address(this), propertyId, shares, "");
         collateralLocked[propertyId] += shares;
 
         _mint(msg.sender, stableCoinAmount);
         emit StablecoinMinted(msg.sender, propertyId, stableCoinAmount);
     }
 
-    function redeemStableCoin(
-        uint256 propertyId,
-        uint256 stableCoinAmount
-    ) external {
-        (, uint256 propertyValue, uint256 totalShares) = rwaToken.properties(
-            propertyId
-        );
+    function redeemStableCoin(uint256 propertyId, uint256 stableCoinAmount) external {
+        (, uint256 propertyValue, uint256 totalShares) = rwaToken.properties(propertyId);
         require(totalShares > 0, "invalid property");
 
         uint256 shareValue = propertyValue / totalShares;
-        uint256 sharesToReturn = (stableCoinAmount * COLLATERAL_RATIO) /
-            shareValue;
+        uint256 sharesToReturn = (stableCoinAmount * COLLATERAL_RATIO) / shareValue;
 
-        require(
-            collateralLocked[propertyId] >= sharesToReturn,
-            "Not enough collateralized RWA shares"
-        );
+        require(collateralLocked[propertyId] >= sharesToReturn, "Not enough collateralized RWA shares");
 
         _burn(msg.sender, stableCoinAmount);
-        rwaToken.safeTransferFrom(
-            address(this),
-            msg.sender,
-            propertyId,
-            sharesToReturn,
-            ""
-        );
+        rwaToken.safeTransferFrom(address(this), msg.sender, propertyId, sharesToReturn, "");
         collateralLocked[propertyId] -= sharesToReturn;
 
         emit StablecoinBurned(msg.sender, stableCoinAmount);
